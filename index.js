87e24994fd239ede137cffa5a0ca4bc49da0cea0
Nguyen Duc <97241208+n2duc@users.noreply.github.com>
2023-05-16 14:52:10 +0700

    array method

--- index.js
+++ index.js
@@ -2,8 +2,67 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var username = 'Ngoc Duc';
 var age = 20;
-console.log({ username: username, age: age });
+// console.log({username, age});
 var buildName = function (name, age) {
     return name + " " + age;
 };
-console.log(buildName(username, age));
+// console.log(buildName(username, age));
+var printName = function (obj) {
+    var _a;
+    // if (obj.lastName !== undefined) {
+    //     console.log(obj.lastName.toUpperCase());
+    // }
+    console.log((_a = obj.lastName) === null || _a === void 0 ? void 0 : _a.toUpperCase());
+};
+// printName({firstName: "Ngoc Duc"});
+// printName({firstName: "Ngoc", lastName: "Duc"});
+var printID = function (id) {
+    if (typeof id === "string") {
+        console.log(id.toUpperCase());
+    }
+    else {
+        console.log(id);
+    }
+};
+var printFood = function (food) {
+    if (Array.isArray(food)) {
+        console.log("Foods: " + food.join(" - "));
+    }
+    else {
+        console.log("Food: " + food);
+    }
+};
+function printPoint(p) {
+    console.log("Location x: " + p.x);
+    console.log("Location y: " + p.y);
+    console.log("Location z: " + p.z);
+}
+// printPoint({x: 3, y:4, z: 5});
+// Array & Object
+var Cars = ["Honda", "Yamaha", "Toyota", "Tesla"];
+var scores = ["one", 1, "two", 2];
+var employees = [
+    { id: 1, name: "Ngoc Duc", dept: "it", salary: 2000 },
+    { id: 2, name: "Hanh Nhan", dept: "test", salary: 1100 },
+    { id: 3, name: "Hat Dua", dept: "test", salary: 800 },
+    { id: 4, name: "Me Xung", dept: "it", salary: 2200 },
+];
+// --- Array with forEach, filter, reduce, find, map
+Cars.forEach(function (car, index) {
+    console.log(index + ": " + car);
+});
+Cars.filter(function (car) {
+    console.log(car.length > 5);
+});
+var salary = employees.map(function (employee) {
+    return employee.salary * 23000;
+});
+console.log(salary);
+var IT = employees.filter(function (employee) {
+    return employee.dept === 'it';
+});
+console.log(IT);
+var AuthorEmp = employees.find(function (employee) {
+    return employee.id > 1;
+});
+console.log(AuthorEmp);
--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
 const username: string = 'Ngoc Duc';
 const age: number = 20;
-console.log({username, age});
+// console.log({username, age});
 
 const buildName = (name: string, age: number) => {
     return name + " " + age;
 };
-console.log(buildName(username, age));
+// console.log(buildName(username, age));
 
 const printName = (obj: {firstName: string, lastName?: string}) => {
     // if (obj.lastName !== undefined) {
@@ -13,8 +13,8 @@ const printName = (obj: {firstName: string, lastName?: string}) => {
     // }
     console.log(obj.lastName?.toUpperCase());
 } 
-printName({firstName: "Ngoc Duc"});
-printName({firstName: "Ngoc", lastName: "Duc"});
+// printName({firstName: "Ngoc Duc"});
+// printName({firstName: "Ngoc", lastName: "Duc"});
 
 const printID = (id: number | string) => {
     if (typeof id === "string") {
@@ -30,21 +30,68 @@ const printFood = (food: string[] | string) => {
         console.log("Food: " + food );
     }
 }
-printID(10);
-printID("10");
+// printID(10);
+// printID("10");
 
+// Type and interface
 type Point = {
     x: number,
     y: number
+}
+type PointB = Point & {
+    z: number,
 }
 interface PointX {
     x: number,
     y: number
 }
-function printPoint(p: Point) {
+interface PointY extends PointX {
+    z: number,
+}
+function printPoint(p: PointY) {
     console.log("Location x: " + p.x)
     console.log("Location y: " + p.y)
+    console.log("Location z: " + p.z)
 }
-printPoint({x: 3, y:4});
+// printPoint({x: 3, y:4, z: 5});
 
+// Array & Object
+const Cars: string[] = ["Honda", "Yamaha", "Toyota", "Tesla"]
+const scores: (string | number)[] = ["one", 1, "two", 2];
+const employees = [
+    { id: 1, name: "Williams", dept: "it", salary: 2000 },
+    { id: 2, name: "Brown", dept: "hr", salary: 1100 },
+    { id: 3, name: "Thomas", dept: "hr", salary: 800 },
+    { id: 4, name: "Roberts", dept: "it", salary: 2200 },
+]
+// --- Array with forEach, filter, reduce, find, map
+const carList = Cars.forEach((car, index) => {
+    return index + ": " +  car;
+})
+const salary = employees.map((employee) => {
+    return employee.salary*23000;
+})
+// console.log(salary);
+const IT = employees.filter((employee) => {
+    return employee.dept === 'it';
+})
+// console.log(IT)
+const AuthorEmp = employees.find((employee) => {
+    return employee.dept === 'it';
+})
+// console.log(AuthorEmp);
+const deptList = [...new Set(employees.map(item => item.dept))]
+// console.log(deptList)
+const deptList1 = deptList.map((item) => {
+    const totalSalary = employees.filter((record) => {
+        return record.dept === item;
+    }).reduce((total, employee) => {
+        return total + employee.salary;
+    }, 0);
+    return {
+        'dept': item,
+        'salary': totalSalary,
+    }
+}, 0)
+console.log(deptList1);
 export {}; //Fix redeclare block-scoped variable
